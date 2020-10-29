9da0ddaf541eb3121212b853538d7edb72cc90c5
Fahim Anzam Dip <dfahimanzam@gmail.com>
2020-10-29 16:06:16 +0600

    Tweaks

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; <a href="https://triangeltech.com/">Triangel Technologies Ltd.</a>{{ date('Y') }}
+        Copyright &copy; <a href="https://triangeltech.com/">Triangle Technology Ltd.</a>{{ date('Y') }}
     </div>
     <div class="footer-right">
         1.0.0
