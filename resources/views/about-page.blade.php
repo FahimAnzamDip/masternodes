778bba86686b12e5507e122fe2f86097c5c0f570
Fahim Anzam Dip <dfahimanzam@gmail.com>
2020-11-04 01:24:07 +0600

    Tweaks

--- resources/views/about-page.blade.php
+++ resources/views/about-page.blade.php
@@ -61,144 +61,14 @@
                     <a class="mfp-iframe video-play-btn" href="https://www.youtube.com/watch?v=GmOzih6I1zs" title="Play Video">
                         <i class="fab fa-youtube"></i>
                     </a>
-                    <span class="heboo-text">BITCON</span>
+                    <span class="heboo-text">POS</span>
                 </div><!-- end about-img-box -->
             </div><!-- end col-lg-5 -->
         </div><!-- end row -->
     </div><!-- end container -->
 </section><!-- end about-area -->
 <!-- ================================
-       START ABOUT AREA
-================================= -->
-
-
-<!-- ================================
-       START TEAM AREA
-================================= -->
-<section class="team-area text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 mx-auto">
-                <div class="sec-heading">
-                    <div class="heading-circle m-x-auto"></div>
-                    <p class="sec__meta">our team member</p>
-                    <h2 class="sec__title m-x-auto">Meet Our Awesome Bitcoin Experts.</h2>
-                </div><!-- end sec-heading -->
-            </div><!-- end col-lg-12 -->
-        </div><!-- end row -->
-        <div class="row team-experts-wrapper">
-            <div class="col-lg-3 col-sm-6">
-                <div class="team-item">
-                    <div class="team-img-box">
-                        <img src="{{ asset('frontend') }}/images/team1.jpg" alt="team image" class="team__img">
-                    </div>
-                    <div class="team-content">
-                        <h3 class="team__title"><a href="team-single.html">adam smith</a></h3>
-                        <span class="team__meta">cEO & founder</span>
-                        <ul class="team__social">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                </div><!-- end team-item -->
-            </div><!-- end col-lg-3 -->
-            <div class="col-lg-3 col-sm-6">
-                <div class="team-item">
-                    <div class="team-img-box">
-                        <img src="{{ asset('frontend') }}/images/team2.jpg" alt="team image" class="team__img">
-                    </div>
-                    <div class="team-content">
-                        <h3 class="team__title"><a href="team-single.html">pam sharon</a></h3>
-                        <span class="team__meta">director</span>
-                        <ul class="team__social">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                </div><!-- end team-item -->
-            </div><!-- end col-lg-3 -->
-            <div class="col-lg-3 col-sm-6">
-                <div class="team-item">
-                    <div class="team-img-box">
-                        <img src="{{ asset('frontend') }}/images/team3.jpg" alt="team image" class="team__img">
-                    </div>
-                    <div class="team-content">
-                        <h3 class="team__title"><a href="team-single.html">richard pam</a></h3>
-                        <span class="team__meta">bitcoin consultant</span>
-                        <ul class="team__social">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                </div><!-- end team-item -->
-            </div><!-- end col-lg-3 -->
-            <div class="col-lg-3 col-sm-6">
-                <div class="team-item">
-                    <div class="team-img-box">
-                        <img src="{{ asset('frontend') }}/images/team4.jpg" alt="team image" class="team__img">
-                    </div>
-                    <div class="team-content">
-                        <h3 class="team__title"><a href="team-single.html">marco wise</a></h3>
-                        <span class="team__meta">bitcoin developer</span>
-                        <ul class="team__social">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                </div><!-- end team-item -->
-            </div><!-- end col-lg-3 -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end team-area -->
-<!-- ================================
-       START TEAM AREA
-================================= -->
-
-<!-- ================================
-       START CLIENTLOGO AREA
-================================= -->
-<section class="clientlogo-area clientlogo-area2">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5">
-                <div class="sec-heading">
-                    <div class="heading-circle"></div>
-                    <p class="sec__meta">they trust us</p>
-                    <h2 class="sec__title">Trusted by Bitcoin Business.</h2>
-                </div><!-- end sec-heading -->
-            </div><!-- end col-lg-6 -->
-            <div class="col-lg-7">
-                <div class="client-logo2">
-                    <div class="client-logo-item">
-                        <img src="{{ asset('frontend') }}/images/client1.png" alt="brand image">
-                    </div><!-- end client-logo-item -->
-                    <div class="client-logo-item">
-                        <img src="{{ asset('frontend') }}/images/client2.png" alt="brand image">
-                    </div><!-- end client-logo-item -->
-                    <div class="client-logo-item">
-                        <img src="{{ asset('frontend') }}/images/client3.png" alt="brand image">
-                    </div><!-- end client-logo-item -->
-                    <div class="client-logo-item">
-                        <img src="{{ asset('frontend') }}/images/client4.png" alt="brand image">
-                    </div><!-- end client-logo-item -->
-                    <div class="client-logo-item">
-                        <img src="{{ asset('frontend') }}/images/client5.png" alt="brand image">
-                    </div><!-- end client-logo-item -->
-                </div><!-- end client-logo -->
-            </div><!-- end col-lg-6 -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end clientlogo-area -->
-<!-- ================================
-       START CLIENTLOGO AREA
+       END ABOUT AREA
 ================================= -->
 
     @include('page-sections.newsletter')
